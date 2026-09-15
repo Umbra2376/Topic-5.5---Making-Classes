2ff8948d1bde43e4decb46ab9797df02b35a64d2
362572158 <362572158@2260I-L29-019>
2026-09-15 13:20:10 -0400

    Add project files.

---
+++ Topic 5.5 - Making Classes/Die.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topic_5._5___Making_Classes
+{
+    public class Die
+    {
+        private int _sides;
+        private int _roll;
+        private Random _generator;
+
+        public Die()
+        {
+            _sides = 6;
+            _generator = new Random();
+            _roll = _generator.Next(1, _sides + 1);
+        }
+
+        //Accessor Properties
+        public int Roll
+        {
+            get { return _roll; }
+        }
+
+        public override string ToString()
+        {
+            return _roll.ToString();
+        }
+
+        public void RollDie()
+        {
+            _roll = _generator.Next(1, _sides + 1);
+        }
+        public void DrawRoll()
+        {
+            if (_roll == 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("-----");
+                Console.WriteLine("|   |");
+                Console.WriteLine("| o |");
+                Console.WriteLine("|   |");
+                Console.WriteLine("-----");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if (_roll == 2)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("-----");
+                Console.WriteLine("|o  |");
+                Console.WriteLine("|   |");
+                Console.WriteLine("|  o|");
+                Console.WriteLine("-----");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            else if (_roll == 3)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("-----");
+                Console.WriteLine("|o  |");
+                Console.WriteLine("| o |");
+                Console.WriteLine("|  o|");
+                Console.WriteLine("-----");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if (_roll == 4)
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("-----");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("|   |");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("-----");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if (_roll == 5)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("| o |");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("-----");
+            }
+            else if (_roll == 6)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("-----");
+            }
+            else
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|   |");
+                Console.WriteLine("| ? |");
+                Console.WriteLine("|   |");
+                Console.WriteLine("-----");
+            }
+        }
+    }
+}
---
+++ Topic 5.5 - Making Classes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topic_5._5___Making_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Die die1 = new Die();
+            Die die2 = new Die();
+
+            die1.RollDie();
+            die1.DrawRoll();
+            die2.RollDie();
+            die2.DrawRoll();
+            int sum = die1.Roll + die2.Roll;;
+
+            if (die1.Roll == die2.Roll && die1.Roll != 1)
+                Console.WriteLine("You rolled a double!");
+            else if (die1.Roll == die2.Roll + 1 || die1.Roll == die2.Roll - 1)
+                Console.WriteLine("You rolled subquential numbers!");
+            else if (sum == 7)
+                Console.WriteLine("You rolled a lucky number!");
+            else if (die1.Roll == 1 && die2.Roll == 1)
+                Console.WriteLine("You rolled snake eyes!");
+            else if (sum % 2 == 0)
+                Console.WriteLine("You rolled an even number!");
+            if (die1.Roll > die2.Roll)
+                Console.WriteLine("Die 1 is greater than Die 2");
+            else if (die1.Roll < die2.Roll)
+                Console.WriteLine("Die 2 is greater than Die 1");
+        }
+    }
+}
